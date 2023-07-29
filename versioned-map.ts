0aa3e2c28a32902c626a33e3e347c3ba8a078c7a
Christos Otarola <christos@Christoss-MacBook-Pro.local>
2023-07-28 21:02:16 -0500

    fix: store operations instead of copies of maps

--- versioned-map.ts
+++ versioned-map.ts
@@ -1,24 +1,47 @@
+interface Operation {
+    name: string,
+    params: { 
+        key: string,
+        value?: string
+    }
+}
 export class VersionedMap {
     private version: number;
     private currentMap: Map<string, string>;
-    private history: Map<number, Map<string, string>>;
+    private history: Operation[];
 
     constructor(){
         this.version = 0;
         this.currentMap = new Map();
-        this.history = new Map();
+        this.history = [];
+    }
+
+    private rebuildMap(version: number){
+        const map = new VersionedMap();
+
+        for (let index = 0; index < version; index++) {
+            const op = this.history[index];
+            map[op.name](op.params.key, op.params.value);
+        }
+
+        return map;
     }
 
     public get(key: string, version?: number){
-        if(version){
-            const previousVersion = this.history.get(version);
+        if(version && version < this.version){
+            const previousVersion = this.rebuildMap(version);
             return previousVersion ? previousVersion.get(key) || null : null;    
         }
+
+        if(version && version > this.version){
+            return null;
+        }
+
         return this.currentMap.get(key) || null;
     }
 
     public put(key: string, value: string){
-        this.history.set(this.version, new Map(this.currentMap));
+        this.history.push({ name: 'put', params: { key, value } });
         this.currentMap.set(key, value);
         this.version++;
         return this.version;
@@ -26,7 +49,7 @@ export class VersionedMap {
 
     public erase(key: string){
         if(this.currentMap.has(key)){
-            this.history.set(this.version, new Map(this.currentMap));
+            this.history.push({ name: 'erase', params: { key } });
             this.currentMap.delete(key);
             this.version++;
             return this.version;
